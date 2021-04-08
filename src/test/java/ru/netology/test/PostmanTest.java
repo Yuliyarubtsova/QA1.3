8c3003f961dabef2426524c0e1fb91fac96cbf55
Yuliya Rubtsova <yuliya.rubcova89@mail.ru>
2021-04-08 21:11:57 +0800

    Changed data

--- src/test/java/ru/netology/test/PostmanTest.java
+++ src/test/java/ru/netology/test/PostmanTest.java
@@ -19,7 +19,7 @@ public class PostmanTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("01.01.2022"));
+                .body("data", equalTo("01.05.2020"));
 
 
     }
